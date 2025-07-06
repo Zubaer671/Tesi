fc768d20381e9e44ebd807c40a5e89c5743318f3
Zubaer671 <zubaer12332@gmail.com>
2025-07-06 18:28:27 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('searchInput').addEventListener('input', function () {
-  const keyword = this.value.toLowerCase();
-  document.querySelectorAll('.app-item').forEach(item => {
-    const name = item.querySelector('p').innerText.toLowerCase();
-    item.style.display = name.includes(keyword) ? 'block' : 'none';
-  });
-});
